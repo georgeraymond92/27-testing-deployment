e6ce30958a760853f7e691a2fa7c74f04b791976
george <geomarray@gmail.com>
2019-01-31 14:43:45 -0800

    tests

--- src/__tests__/src/components/counter.test.js
+++ src/__tests__/src/components/counter.test.js
@@ -1,14 +1,32 @@
 import React from "react";
 import renderer from "react-test-renderer";
 import Counter from "../../../components/counter/counter.js";
+import { shallow, mount, render } from 'enzyme';
 
 describe("<Counter />", () => {
-  it("is alive", () => {
-    // let component = shallow(<Counter />);
-    // expect(component.find('span')).toBeFalsy();
-    expect(true).toBeTruthy();
+  it("should return false", () => {
+    let component = shallow(<Counter />);
+    expect(component.find('span').exists()).toBeTruthy();
   });
+  it('Should increment the state', () => {
+    let component = mount(<Counter />);
+    let button = component.find('.btn-green');
+    button.simulate('click');
+    expect(component.state('count')).toEqual(1);
+  })
+  it('Should decrement the state', () => {
+    let component = mount(<Counter />);
+    let button = component.find('.btn-red');
+    button.simulate('click');
+    expect(component.state('count')).toEqual(-1);
+  })
 
+  it('renders correctly', () => {
+    const tree = renderer.create(<Counter />).toJSON();
+    expect(tree).toMatchSnapshot();
+  })
+
+  
 
 
 });
